63c122fc0414b332165bc183132b6f3b849f8665
imcousin <imcousin@users.noreply.github.com>
2022-04-11 17:05:29 -0700

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,6 +57,6 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  # NOT WORKING IN M1
+  # This issue comes because the M1 arm docker is using qemu and this doesn't support inotify. NOT WORKING IN M1
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
